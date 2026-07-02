da154499b2a0814927bee2e4c81478db481bf3dc
Tannhuot KHOUV <khouvtannhuot@gmail.com>
2026-07-02 19:27:29 +0700

    ui basic

---
+++ app/src/main/java/com/example/demo_e18/LoginActivity.java
@@ -0,0 +1,92 @@
+package com.example.demo_e18;
+
+import android.content.Context;
+import android.hardware.input.InputManager;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextInputLayout etUsernameLayout;
+    TextInputEditText etUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ConstraintLayout mainLayout = findViewById(R.id.main);
+        mainLayout.setOnClickListener(v -> {
+            hideKeyboard();
+        });
+
+        etUsername = findViewById(R.id.etUsername);
+        etUsernameLayout = findViewById(R.id.etUsernameLayout);
+
+        etUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("ET Username", s.toString());
+            }
+        });
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v -> {
+            String username = Objects.requireNonNull(etUsername.getText()).toString().trim();
+
+            if (username.isEmpty()) {
+                etUsernameLayout.setError("Username required");
+            } else {
+                etUsernameLayout.setError(null);
+            }
+
+            hideKeyboard();
+        });
+    }
+
+    private void hideKeyboard() {
+        View view = this.getCurrentFocus();
+
+        if (view != null) {
+            view.clearFocus();
+
+            InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (inputManager != null) {
+                inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/demo_e18/MainActivity.java
+++ app/src/main/java/com/example/demo_e18/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.demo_e18;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     CheckBox cb2;
 
     RadioGroup rdGroupGender;
+    ProgressBar loadingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +61,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         cb2 = findViewById(R.id.cb2);
         rdGroupGender = findViewById(R.id.rdGroupGender);
 
+        loadingView = findViewById(R.id.loadingView);
+
         tvHello.setText("Hello world!");
 
 //        btn1.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +83,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         ImageButton imgBtn = findViewById(R.id.btnHome);
         imgBtn.setOnClickListener(this);
+
+        handleCheckboxValue();
+        handleRadioButton();
+    }
+
+    private void handleCheckboxValue() {
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "You select Checkbox 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cb2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "You select Checkbox 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void handleRadioButton() {
+        rdGroupGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup group, int checkedId) {
+                RadioButton rdButton = findViewById(checkedId);
+
+                if (rdButton != null) {
+                    String value = rdButton.getText().toString();
+                    Toast.makeText(MainActivity.this, "Gender: " + value, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -93,7 +134,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         }
                     });
             snackBar.show();
+        } else if (id == R.id.btn3) {
+            loadingView.setVisibility(View.VISIBLE);
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            );
+
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                loadingView.setVisibility(View.GONE);
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                btn1.setEnabled(true);
+            }, 2000);
         } else if (id == R.id.btnHome) {
+            Log.d("Checkbox", "Checkbox 1: " + cb1.isChecked());
+            Log.d("Checkbox", "Checkbox 2: " + cb2.isChecked());
+
+            int selectedRdID = rdGroupGender.getCheckedRadioButtonId();
+            RadioButton rdButton = findViewById(selectedRdID);
+
+            if (rdButton != null) {
+                String value = rdButton.getText().toString();
+                Toast.makeText(MainActivity.this, "Gender: " + value, Toast.LENGTH_SHORT).show();
+            }
+
             Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show();
         }
     }
